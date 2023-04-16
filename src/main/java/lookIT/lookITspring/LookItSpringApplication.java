ea4f695a862ad255c7acfdbb56ab44ddfed70718
whitez1 <heeji0502@gmail.com>
2023-04-16 21:33:11 +0900

    Initial commit

---
+++ src/main/java/lookIT/lookITspring/LookItSpringApplication.java
@@ -0,0 +1,13 @@
+package lookIT.lookITspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookItSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookItSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lookIT/lookITspring/LookItSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package lookIT.lookITspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookItSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
