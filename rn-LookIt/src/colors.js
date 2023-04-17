9f606eefbe65f83dd0613149f9fdbb3bf6b93cac
Minho Lim <minho119@ajou.ac.kr>
2023-04-17 20:42:48 +0900

    fix : Change hex color

--- rn-LookIt/src/colors.js
+++ rn-LookIt/src/colors.js
@@ -1,24 +1,75 @@
-export const WHITE = '#ffffff';
+// LIGHT : 활성화되지 않은 색상
+// DEFAULT : 일반적으로 사용하는 색상
+// DARK : 강조하고 싶은 색상
 
-export const BLACK = '#000000';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#111111';
 
 export const TRANSPARENT = {
   LIGHT: 'transparent',
   DEFAULT: 'transparent',
   DARK: 'transparent',
 };
+
+export const GRAY = {
+  DEFAULT: '#B4B4B4',
+  LIGHT: '#979797',
+  50: '#F2F2F2',
+  100: '#ECECEC',
+  200: '#E6E6E6',
+  300: '#D8D8D8',
+  400: '#B4B4B4',
+  500: '#979797',
+  600: '#6E6E6E',
+  700: '#5B5B5B',
+  800: '#3C3C3C',
+  900: '#1C1C1C',
+};
+
 export const DANGER = {
-  LIGHT: '#fca5a5',
-  DEFAULT: '#dc2626',
-  DARK: '#7f1d1d',
+  LIGHT: '#EBC5C7',
+  DEFAULT: '#DE5145',
+  DARK: '#A51C10',
+  50: '#ECE2E3',
+  100: '#EBC5C7',
+  200: '#DC928F',
+  300: '#D26D69',
+  400: '#DE5145',
+  500: '#E1412B',
+  600: '#D2382A',
+  700: '#C02B24',
+  800: '#B3261E',
+  900: '#A51C10',
 };
 
 export const PRIMARY = {
-  DEFAULT: '#6e80e6',
-  DARK: '#4b4dac',
-  LIGHT: '#a8b7fa',
+  LIGHT: '#D3D0FF',
+  DEFAULT: '#4750A8',
+  DARK: '#2B2771',
+  50: '#FFE4FF',
+  100: '#D3D0FF',
+  200: '#BFB2FF',
+  300: '#A296F5',
+  400: '#9183E3',
+  500: '#7A6ECF',
+  600: '#6760BE',
+  700: '#4750A8',
+  800: '#433A8B',
+  900: '#2B2771',
 };
-export const GRAY = {
-  DEFAULT: '#a3a3a3',
-  LIGHT: '#e5e5e5',
+
+export const SECONDARY = {
+  LIGHT: '#CEDAFF',
+  DEFAULT: '#6E80E6',
+  DARK: '#4B4DAC',
+  50: '#FFFFFF',
+  100: '#FAFFFF',
+  200: '#CEDAFF',
+  300: '#A8B7FA',
+  400: '#899AEF',
+  500: '#6E80E6',
+  600: '#6978DD',
+  700: '#616DCF',
+  800: '#5963C3',
+  900: '#4B4DAC',
 };
