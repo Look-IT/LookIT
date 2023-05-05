7d87fc4b78cba6e14418f24a3ee432af8c620a1c
Junho Choi <tig9659@ajou.ac.kr>
2023-05-05 22:40:34 +0900

    chore: backend merge // friendRepository Err

--- src/main/java/lookIT/lookITspring/entity/Memory.java
+++ src/main/java/lookIT/lookITspring/entity/Memory.java
@@ -21,36 +21,5 @@ public class Memory {
     @Column(name = "userID", nullable = false)
     private Integer userId;
 
-    // constructors, getters, and setters
-    public Memory() {
-    }
-
-    public Memory(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Integer getMemoryId() {
-        return memoryId;
-    }
-
-    public void setMemoryId(Integer memoryId) {
-        this.memoryId = memoryId;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
 }
 
