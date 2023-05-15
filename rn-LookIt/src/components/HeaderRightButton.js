5a6897b2098d982292ab2713fdf94547d4e2b634
shg9611 <shg9611@naver.com>
2023-05-15 18:43:30 +0900

    바텀시트 컴포넌트 구현 완료

--- rn-LookIt/src/components/HeaderRightButton.js
+++ rn-LookIt/src/components/HeaderRightButton.js
@@ -1,19 +1,96 @@
-import { Pressable, Image } from 'react-native';
+//내 프로필 헤더 오른쪽 버튼 (로그아웃 용도)
+
+import {
+  Pressable,
+  Image,
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+} from 'react-native';
+import { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { BLACK, DANGER, GRAY } from '../colors';
+import { useUserContext } from '../contexts/UserContext';
+import RBSheet from 'react-native-raw-bottom-sheet';
+
 const HeaderRightButton = ({ tintColor }) => {
+  const { user, setUser } = useUserContext(); // 현재 로그인 정보 받아오는 상태변수
+  const refRBSheet = useRef(); // bottom sheet의 ref 상태 변수
+  const [visible, setVisible] = useState(false); // 모달 창의 보임 여부를 나타내는 상태 변수
+
+  const onLogout = () => {
+    //로그아웃 버튼 클릭시 호출되는 함수
+    setUser(null);
+
+    console.log(user);
+  };
+
   return (
-    <Pressable hitSlop={10} onPress={() => {}}>
-      <Image
-        source={require('../../assets/Icon_More.png')}
-        style={{ width: 24, height: 24 }}
-      ></Image>
-    </Pressable>
+    <>
+      <Pressable hitSlop={10} onPress={() => refRBSheet.current.open()}>
+        <Image
+          source={require('../../assets/Icon_More.png')}
+          style={styles.image}
+        ></Image>
+      </Pressable>
+
+      <RBSheet
+        ref={refRBSheet}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        height={92}
+        animationType="fade"
+        customStyles={{
+          wrapper: {
+            backgroundColor: '#00000045',
+          },
+          draggableIcon: {
+            backgroundColor: GRAY[700],
+          },
+          container: {
+            width: '100%',
+            paddingHorizontal: 20,
+            borderTopStartRadius: 28,
+            borderTopEndRadius: 28,
+          },
+        }}
+      >
+        <TouchableOpacity onPress={onLogout}>
+          <View style={styles.bottomSheetList}>
+            <Image
+              source={require('../../assets/Icon_Logout.png')}
+              style={[styles.image, { marginRight: 8 }]}
+            ></Image>
+            <Text style={styles.text}>로그아웃</Text>
+          </View>
+        </TouchableOpacity>
+      </RBSheet>
+    </>
   );
 };
 
 HeaderRightButton.propTypes = {
   tintColor: PropTypes.string,
 };
+
+const styles = StyleSheet.create({
+  bottomSheetList: {
+    width: '100%',
+    height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 24,
+    height: 24,
+  },
+  text: {
+    fontSize: 16,
+    color: DANGER.DEFAULT,
+  },
+});
 
 export default HeaderRightButton;
--- rn-LookIt/src/screens/SignUpScreen.js
+++ rn-LookIt/src/screens/SignUpScreen.js
@@ -1,12 +1,12 @@
 //회원가입 화면
 
-import { Keyboard, StyleSheet, Text, View } from 'react-native';
+import { Keyboard, StyleSheet, Text, View, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Input, { KeyboardTypes, ReturnKeyTypes } from '../components/Input';
 import { useEffect, useState } from 'react';
 import Button, { ButtonTypes } from '../components/Button';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-
+import { signUp } from '../api/auth';
 import TextButton from '../components/TextButton';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
@@ -14,7 +14,7 @@ const SignUpScreen = () => {
   const navigation = useNavigation();
   const { top, bottom } = useSafeAreaInsets(); //화면 위아래 겹치거나 잘리는 경우 방지를 위한 상태변수
 
-  const [id, setId] = useState(''); //아이디 저장
+  const [tagName, setTagName] = useState(''); //아이디 저장
   const [email, setEmail] = useState(''); //이메일 저장
   const [password, setPassword] = useState(''); //패스워드 저장
   const [nickName, setNickName] = useState(''); //닉네임 저장
@@ -24,16 +24,44 @@ const SignUpScreen = () => {
   const [disabled, setDisabled] = useState(true); //입력 덜 된채로 버튼 클릭 방지
 
   useEffect(() => {
-    setDisabled(!email || !password || !id || !nickName || !idConfirm);
-  }, [email, password, id, nickName, idConfirm]);
+    setDisabled(!email || !password || !tagName || !nickName || !idConfirm);
+  }, [email, password, tagName, nickName, idConfirm]);
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     //회원가입 버튼 클릭시 호출되는 함수
     Keyboard.dismiss();
     if (!disabled && !isLoading) {
-      setIsLoading(true);
-      console.log(id, email, password, nickName);
-      setIsLoading(false);
+      try {
+        setIsLoading(true);
+        console.log(tagName, email, password, nickName);
+
+        const response = await signUp(
+          'https://port-0-lookit-f69b2mlh8tij3t.sel4.cloudtype.app/member/join',
+          tagName,
+          email,
+          password,
+          nickName
+        );
+        console.log(response.data);
+        setIsLoading(false);
+        if (response.data) {
+          navigation.navigate('SighIn');
+        } else {
+          throw new Error(
+            '회원가입 실패: 서버로부터 잘못된 응답을 받았습니다.'
+          );
+        }
+      } catch (error) {
+        console.log(error);
+
+        Alert.alert('회원가입 실패', '회원가입이 실패했습니다.', [
+          {
+            text: '확인',
+            style: 'default',
+            onPress: () => setIsLoading(false),
+          },
+        ]);
+      }
     }
   };
 
@@ -41,6 +69,7 @@ const SignUpScreen = () => {
     //아이디 중복 확인 버튼 클릭시 호출되는 함수
 
     setIdConfirm(true);
+    console.log(idConfirm);
   };
   return (
     <KeyboardAwareScrollView contentContainerStyle={{ flexGrow: 1 }}>
@@ -52,8 +81,8 @@ const SignUpScreen = () => {
             <Input
               title={'아이디'}
               placeholder="아이디를 입력해주세요."
-              value={id}
-              onChangeText={(id) => setId(id.trim())}
+              value={tagName}
+              onChangeText={(tagName) => setTagName(tagName.trim())}
               returnKeyType={ReturnKeyTypes.NEXT}
               KeyboardType={KeyboardTypes.DEFAULT}
             ></Input>
