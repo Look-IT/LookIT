c31064da0757ebafec88b7da6f694f7851688855
Minho Lim <minho119@ajou.ac.kr>
2023-05-28 17:39:31 +0900

    feat: add information view by landmark

--- rn-LookIt/src/components/navermap/LandmarkBottomSheet.js
+++ rn-LookIt/src/components/navermap/LandmarkBottomSheet.js
@@ -1,10 +1,32 @@
-import { useRef } from "react"
+import { useEffect, useState } from "react"
 import RBSheet from "react-native-raw-bottom-sheet"
 import { GRAY } from "../../colors";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { getLandmarkInfo } from "../../api/landmarkInfo";
+import { Family, Title } from "../../styles/fonts";
+import { Body } from "../../styles/fonts";
 
 const LandmarkBottomSheet = ({landmarkId, refRBSheet, setSelectedLandmark}) => {
+
+  const [landmarkData, setLandmarkData] = useState(null);
   
+  useEffect(() => {
+    if (landmarkId === null) {
+      return;
+    }
+    
+    getLandmarkInfo(landmarkId)
+      .then(landmarkData => {
+        console.log(landmarkData);
+        setLandmarkData(landmarkData);
+      })
+      .catch(error => console.error(error));
+
+  }, [landmarkId]);
+
+  useEffect(() => {
+    console.log(landmarkData);
+  }, [landmarkData]);
 
   return (
     <RBSheet
@@ -26,14 +48,62 @@ const LandmarkBottomSheet = ({landmarkId, refRBSheet, setSelectedLandmark}) => {
             },
           }}
         >
-          <View>
-            <Text>
-              {landmarkId}
-            </Text>
-          </View>
+          {
+            landmarkData && (
+              <View>
+
+                <View>
+                  <Text style={landmarkInfoStyle.title}>
+                    {landmarkData.landmarkName}
+                  </Text>
+                  <Text style={landmarkInfoStyle.address}>
+                    {"주소주소주소주소주소"}
+                  </Text>
+                  <Text style={landmarkInfoStyle.info}>
+                    {landmarkData.landInfo}
+                  </Text>
+                </View>
+
+                <View>
+                  <Image style={landmarkInfoStyle.FrameImage}
+                    source={{ uri: landmarkData.frameUrl }}
+                  />
+                </View>
+              
+              </View>
+            )
+          }
 
     </RBSheet>
   )
 }
+
+const landmarkInfoStyle = StyleSheet.create({
+  container: {
+
+  },
+  title: {
+    ...Family.KR_Medium,
+    ...Title.Medium,
+    color: GRAY['900'],
+    marginBottom: 8,
+  },
+  address: {
+    ...Family.KR_Regular,
+    ...Body.Small,
+    color: GRAY['500'],
+    marginBottom: 24,
+  },
+  info: {
+    ...Family.KR_Regular,
+    ...Body.Medium,
+    color: GRAY['700'],
+  },
+  FrameImage: {
+    width: 128,
+    height: 192,
+    marginLeft: 16,
+  }
+})
 
 export default LandmarkBottomSheet;
