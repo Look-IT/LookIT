73d164e87ba7252cf710c88bd1aa359e340458f7
Hyojin <snowfluppy@ajou.ac.kr>
2023-06-05 01:09:09 +0900

    chore: give authorization to memory friend tag
    - allows tagging friend to memory by giving authorization

--- backend/src/main/java/lookIT/lookITspring/security/SecurityConfig.java
+++ backend/src/main/java/lookIT/lookITspring/security/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers("/memories/info").permitAll()
 			.antMatchers("/memories/info/delete").permitAll()
 			.antMatchers("/memories/list").permitAll()
+			.antMatchers("/memories/friendList").permitAll()
+			.antMatchers("/memories/friendTag").permitAll()
 			.antMatchers("/memories/taggedFriendList").permitAll()
 			.antMatchers("/main").permitAll()
 			.antMatchers("/main/landmarks").permitAll()
