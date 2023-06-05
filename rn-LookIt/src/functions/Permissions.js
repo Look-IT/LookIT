f95dcc84197c9adfb89fc749eb22eb3ae75b53cc
Minho Lim <minho119@ajou.ac.kr>
2023-06-05 21:19:53 +0900

    chore: modify permissions

--- rn-LookIt/src/functions/Permissions.js
+++ rn-LookIt/src/functions/Permissions.js
@@ -1,5 +1,8 @@
 import { PERMISSIONS, RESULTS, checkMultiple, requestMultiple } from "react-native-permissions"
 import { Platform } from "react-native";
+// import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import VIForegroundService from "@voximplant/react-native-foreground-service";
 
 export const requestPermissions = async () => {
   if (Platform.OS !== 'android') return;
@@ -8,6 +11,7 @@ export const requestPermissions = async () => {
     PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
     PERMISSIONS.ANDROID.READ_MEDIA_IMAGES,
     PERMISSIONS.ANDROID.CAMERA,
+    PERMISSIONS.ANDROID.POST_NOTIFICATIONS,
   ]);
 
   const deniedPermissions = Object.entries(statuses)
@@ -18,4 +22,13 @@ export const requestPermissions = async () => {
     const requestStatues = await requestMultiple(deniedPermissions);
     console.log(requestStatues);
   }
+
+  const channelConfig = {
+      id: 'default',
+      name: 'default',
+      description: '추억일지 기록 관련 알림 채널',
+      importance: 4,
+      enableVibration: false
+    };
+    await VIForegroundService.getInstance().createNotificationChannel(channelConfig);
 }
