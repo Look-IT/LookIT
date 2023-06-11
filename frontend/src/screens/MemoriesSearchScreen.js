e6602d4dcc910c5032ebc94edfaa5c49988d1488
Minho Lim <minho119@ajou.ac.kr>
2023-06-11 22:46:09 +0900

    style: modify memoriesSearchScreen design

--- frontend/src/screens/MemoriesSearchScreen.js
+++ frontend/src/screens/MemoriesSearchScreen.js
@@ -1,54 +1,60 @@
-import { Image, Platform, Pressable, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
 import { BLACK } from "../colors";
-import FriendAddInput, { KeyboardTypes, ReturnKeyTypes } from "../components/FriendAddInput";
-import { useState } from "react";
+import { KeyboardTypes, ReturnKeyTypes } from "../components/FriendAddInput";
+import { useCallback, useState } from "react";
 import { getSearchMemories } from "../api/DiaryApi";
 import DiaryList from "../components/DiaryList";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import MemoriesSearchInput from "../components/MemoriesSearchInput";
 
 const MemoriesSearchScreen = () => {
   const [infoTag, setInfoTag] = useState('');
   const [searchDiaryList, setSearchDiaryList] = useState([]);
   const navigation = useNavigation();
 
+  useFocusEffect(
+    useCallback(() => {
+      handleGetDiary();
+    }, [])
+  )
+
+  const handleGetDiary = () => {
+    getSearchMemories(infoTag)
+      .then(response => {
+        setSearchDiaryList(
+          response.map((diaryObj) => {
+            return {
+              id: diaryObj.memoryId,
+              date: diaryObj.createAt,
+              thumbnail: diaryObj.memoryPhoto,
+              tag: diaryObj.info,
+              friends: diaryObj.friends,
+            };
+          })
+        )
+      })
+      .catch(error => console.error(error))
+  }
+
   return (
     
     <View style={styles.container}>
 
       <View style={styles.headerContainer}>
-        <Pressable onPress={() => navigation.goBack()}>
-          <Image
-            style={styles.leftIcon}
-            source={require('../../assets/Icon_Back.png')}/>
-        </Pressable>
-      </View>
 
-      <FriendAddInput
-        title={''}
-        placeholder="검색하고자 하는 정보태그를 입력해주세요"
-        keyboardType={KeyboardTypes.DEFAULT}
-        returnKeyType={ReturnKeyTypes.DONE}
-        value={infoTag}
-        onChangeText={(inputData) => {
-          setInfoTag(inputData.trim())
-        }}
-        onSubmitEditing={() => {
-          getSearchMemories(infoTag)
-            .then(response => {
-              setSearchDiaryList(
-                response.map((diaryObj) => {
-                  return {
-                    id: diaryObj.memoryId,
-                    date: diaryObj.createAt,
-                    thumbnail: diaryObj.memoryPhoto,
-                    tag: diaryObj.info,
-                    friends: diaryObj.friends,
-                  };
-                })
-              )
-            })
-            .catch(error => console.error(error))
-        }}/>
+        <MemoriesSearchInput
+          title={''}
+          placeholder="정보태그를 입력해주세요"
+          keyboardType={KeyboardTypes.DEFAULT}
+          returnKeyType={ReturnKeyTypes.DONE}
+          value={infoTag}
+          onChangeText={(inputData) => {
+            setInfoTag(inputData.trim())
+          }}
+          onSubmitEditing={() => {
+            handleGetDiary();
+          }}/>
+      </View>
 
         <DiaryList data={searchDiaryList} style={{width: '100%'}} />
 
@@ -64,7 +70,8 @@ const styles = StyleSheet.create({
   headerContainer: {
     height: 56,
     paddingHorizontal: 16,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+
   },
   leftIcon: {
     width: 24,
